7458f7e69258ed6233b7fe1427d2d0260c9dd9be
Sam Hodge <102564797+samhodge-aiml@users.noreply.github.com>
2025-03-11 23:19:31 +0000

    glitch

--- videohelpersuite/server.py
+++ videohelpersuite/server.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import re
 from comfy.cmd import server
+from comfy.cmd import folder_paths
 import asyncio
 
 from .utils import is_url, get_sorted_dir_files_from_directory, ffmpeg_path, \
